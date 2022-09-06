c0790d2a6aa2f3819d172292e7800be86daf0b0d
ilyass <ex_p@live.com>
2022-09-06 10:56:56 +0300

    tower of hanoi

--- main.py
+++ main.py
@@ -1,43 +1,81 @@
-import time, sys
+import copy
+import sys
 
-try:
-    import bext
-except ImportError:
-    print("you need bext module")
-    sys.exit()
+TOTAL_DISKS = 5
 
-print('Press Ctrl-C to stop.')
-time.sleep(3)
+COMPLETE_TOWER = list(range(TOTAL_DISKS,0,-1))
 
-indent = 0
-indentIncreasing = True
+def main():
+    print("""The Tower of Hanoi game""")
 
-try:
+    towers = {'A': copy.copy(COMPLETE_TOWER),'B': [], 'C': []}
+
     while True:
-        print(' ' * indent, end=' ')
-        bext.fg("red")
-        print("##", end=" ")
-        bext.fg("yellow")
-        print('##', end=' ')
-        bext.fg("green")
-        print('##', end=' ')
-        bext.fg("blue")
-        print('##', end=' ')
-        bext.fg("cyan")
-        print('##', end=' ')
-        bext.fg("purple")
-        print("##", end=" ")
+        displayTowers(towers)
 
-        if indentIncreasing:
-            indent += 1
-            if indent == 60:
-                indentIncreasing = False
+        fromTower, toTower = askForPlayerMove(towers)
+
+        disk = towers[fromTower].pop()
+        towers[toTower].append(disk)
+
+        if COMPLETE_TOWER in (towers['B'], towers['C']):
+            displayTowers(towers)
+            print('You have solved the puzzle. Bravo! ')
+            sys.exit()
+
+def askForPlayerMove(towers):
+    """Asks the player for a move. Returns (fromTower, toTower)."""
+
+    while True:
+        print('Enter the letters of "from" and "to" towers, or QUIT.')
+        print('(e.g. AB to moves a disk from tower A to tower B.)')
+        response = input('> ').upper().strip()
+
+        if response == "QUIT":
+            print("Thanks for playing")
+            sys.exit()
+
+        if response not in ('AB', 'AC', 'BA', 'BC', 'CA', 'CB'):
+            print('Enter one of AB, AC, BA, BC, CA, or CB.')
+            continue
+
+        fromTower, toTower = response[0], response[1]
+
+        if len(towers[fromTower]) == 0:
+            print('You selected a tower with no disks.')
+            continue
+        elif len(towers[toTower]) == 0:
+            return fromTower, toTower
+        elif towers[toTower][-1] < towers[fromTower][-1]:
+            print('Can\'t put larger disks on top of smaller ones.')
+            continue
         else:
-            indent -= 1
-            if indent == 0:
-                indentIncreasing = True
+            return fromTower, toTower
 
-        time.sleep(0.2)
-except KeyboardInterrupt:
-    sys.exit()
+def displayTowers(towers):
 
+    for level in range(TOTAL_DISKS,-1,-1):
+        for tower in (towers['A'], towers['B'], towers['C']):
+            if level >= len(tower):
+                displayDisk(0)
+            else:
+                displayDisk(tower[level])
+        print()
+
+    emptySpace = ' ' * (TOTAL_DISKS)
+    print('{0} A{0}{0} B{0}{0} C\n'.format(emptySpace))
+
+
+def displayDisk(width):
+
+    emptySpace = ' ' * (TOTAL_DISKS-width)
+
+    if width == 0:
+        print(emptySpace+ '||' + emptySpace,end='')
+    else:
+        disk = '@' * width
+        numLabel= str(width).rjust(2, '_')
+        print(emptySpace+ disk+ numLabel+ disk + emptySpace, end="")
+
+if __name__ == "__main__":
+    main()
